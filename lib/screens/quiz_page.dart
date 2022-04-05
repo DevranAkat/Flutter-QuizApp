80a3138b9ea8c5b4e32d3e87213aabd0ba0df0be
DevranAkat <devran.akat@hotmail.com>
2022-04-06 00:30:10 +0200

    Added options and designed image

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -5,32 +5,49 @@ class QuizPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Color(0xFF222831),
-          title: const Text('Quiz'),
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(40),
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xFF222831),
+        title: const Text('Quiz'),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(40),
           ),
         ),
-        body: Column(
-          children: [
-            Card(
-              child: Container(
-                height: 200,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0), image: DecorationImage(fit: BoxFit.contain, image: AssetImage('assets/image.png'))),
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text('Dette er spørsmålet'),
-                ),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Container(
+              height: 200,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                image: const DecorationImage(fit: BoxFit.fill, image: AssetImage('assets/image.png')),
+                borderRadius: BorderRadius.circular(20),
               ),
-              margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-            )
-          ],
-        ));
+            ),
+            color: Colors.transparent,
+            elevation: 0.0,
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 5.0),
+          ),
+          Options("Answer 1"),
+          Options("Answer 2"),
+          Options("Answer 3"),
+          Options("Answer 4"),
+        ],
+      ),
+    );
   }
+}
+
+Padding Options(String answer) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+    child: ElevatedButton(
+      onPressed: () => {},
+      child: Text(answer, style: const TextStyle(color: Colors.black)),
+      style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50), primary: Colors.white, textStyle: const TextStyle(fontSize: 22), padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 50)),
+    ),
+  );
 }
