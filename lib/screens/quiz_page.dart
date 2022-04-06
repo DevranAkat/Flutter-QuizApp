3eac8d41134d16d36bac782ade59e324c12d4266
DevranAkat <devran.akat@hotmail.com>
2022-04-06 11:12:35 +0200

    Update quiz_page.dart

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -15,27 +15,32 @@ class QuizPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Card(
-            child: Container(
-              height: 200,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 220,
               width: double.infinity,
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
               decoration: BoxDecoration(
                 color: Colors.transparent,
-                image: const DecorationImage(fit: BoxFit.fill, image: AssetImage('assets/image.png')),
-                borderRadius: BorderRadius.circular(20),
+                image: const DecorationImage(fit: BoxFit.cover, image: AssetImage('assets/image.png')),
+                borderRadius: BorderRadius.circular(15),
               ),
             ),
-            color: Colors.transparent,
-            elevation: 0.0,
-            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 5.0),
-          ),
-          Options("Answer 1"),
-          Options("Answer 2"),
-          Options("Answer 3"),
-          Options("Answer 4"),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+                style: const TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            Options("Answer 1"),
+            Options("Answer 2"),
+            Options("Answer 3"),
+            Options("Answer 4"),
+          ],
+        ),
       ),
     );
   }
@@ -47,7 +52,15 @@ Padding Options(String answer) {
     child: ElevatedButton(
       onPressed: () => {},
       child: Text(answer, style: const TextStyle(color: Colors.black)),
-      style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(50), primary: Colors.white, textStyle: const TextStyle(fontSize: 22), padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 30)),
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size.fromHeight(50),
+        primary: Colors.white,
+        textStyle: const TextStyle(fontSize: 22),
+        padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 30),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
     ),
   );
 }
