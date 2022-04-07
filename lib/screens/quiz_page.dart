686ff9b9fc77c86598f7172aa2d68689cb7f7a83
DevranAkat <devran.akat@hotmail.com>
2022-04-08 01:31:18 +0200

    Get Q from List

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -1,8 +1,45 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
+const List questions = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "A memory location that holds a single letter or number.",
+    "options": ['Double', 'Int', 'Char', 'Word'],
+    "answer_index": 2,
+  },
+  {
+    "id": 4,
+    "question": "What command do you use to output data to the screen?",
+    "options": ['Cin', 'Count>>', 'Cout', 'Output>>'],
+    "answer_index": 2,
+  },
+];
+
+final String q1 = questions[0]['question'];
+
+String xx =  questions[0]['question'];
+  String getQuestion()
+  {
+    return questions[0]['question'];
+  }
+
 class QuizPage extends StatelessWidget {
   const QuizPage({Key? key}) : super(key: key);
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,17 +66,17 @@ class QuizPage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(15),
               ),
             ),
-            const Padding(
-              padding:  EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
-                child:  AutoSizeText(
-                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+                child: AutoSizeText(
+                  questions[0]['question'],
                   style: TextStyle(fontSize: 24, color: Colors.white),
                   minFontSize: 14,
                   maxLines: 4,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
-            Options("Answer 1, Lorem Ipsum is simply dummy text of the"),
+            Options("Answer 1, Lorem asd  da sd asd sad ssssss ddddd dsadsa Lorem asd  da sd asd sad ssssss ddddd aadsad ddsadsad Ipsum is simply dummy text of the"),
             Options("Answer 2"),
             Options("Answer 3"),
             Options("Answer 4"),
@@ -58,11 +95,11 @@ Padding Options(String answer) {
       child: AutoSizeText(
         answer,
         style: const TextStyle(
-          fontSize: 18, color: Color(0xFF222831),
+          fontSize: 22, color: Color(0xFF222831),
           fontWeight: FontWeight.bold),
-          minFontSize: 12,
+          minFontSize: 10,
           maxLines: 3,
-          overflow: TextOverflow.ellipsis,
+          overflow: TextOverflow.visible,
       ),
       style: ElevatedButton.styleFrom(
         minimumSize: const Size.fromHeight(50),
